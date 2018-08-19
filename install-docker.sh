054b9997e4871e09352a59df5fbc6cfa4618f446
Jezrael Arciaga <jezarciaga@gmail.com>
2018-08-19 08:56:03 +0800

    Add new script install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+# Install docker in AWS AMI
+
+sudo yum update -y && \
+    sudo yum install docker -y && \
+    sudo service docker start && \
+    sudo usermod -a -G docker ec2-user && \
+    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` \
+	 -o /usr/local/bin/docker-compose && \
+    sudo chmod +x /usr/local/bin/docker-compose
