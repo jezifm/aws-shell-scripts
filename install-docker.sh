7428680d3fa6224b4eb9dc05fc97ab4457650394
Jezrael Arciaga <jezarciaga@gmail.com>
2018-08-21 13:25:07 +0800

    Fix issue unable to resolve host, docker not running http+socket

--- install-docker.sh
+++ install-docker.sh
@@ -6,7 +6,8 @@ IFS=$'\n\t'
 sudo yum update -y && \
     sudo yum install docker -y && \
     sudo service docker start && \
-    sudo usermod -a -G docker ec2-user && \
-    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` \
-	 -o /usr/local/bin/docker-compose && \
-    sudo chmod +x /usr/local/bin/docker-compose
+    sudo usermod -a -G docker ec2-user;
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` \
+     -o /usr/local/bin/docker-compose;
+sudo chmod +x /usr/local/bin/docker-compose;
+sudo chown $USER:docker /var/run/docker.sock
